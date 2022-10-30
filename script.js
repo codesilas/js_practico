40977cb5198cda900fdc27361d5b9d6a1a26127a
Silas Duarte <silas.duarte.h@gmail.com>
2022-10-29 23:04:24 -0600

    se añade html y js para practica de manipulación del DOM

---
+++ script.js
@@ -0,0 +1,5 @@
+const h1 = document.querySelector("h1");
+const input1 = document.querySelector('#calculo1');
+const input1 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+
